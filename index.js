91457c88c9b00923a6ee83a34ff10b7062afad6c
Abhijeet Singh <aps.120797@gmail.com>
2017-10-21 22:14:13 +0530

    Implemented basic design using Electron

---
+++ index.js
@@ -0,0 +1,17 @@
+const {BrowserWindow, app} = require('electron');
+
+let win;
+
+let boot = () => {
+    win = new BrowserWindow({
+        height: 480,
+        width: 640,
+        frame: false
+    });
+
+    win.loadURL(`file://${__dirname}/index.html`);
+    win.webContents.openDevTools();
+    
+};
+
+app.on('ready', boot);
